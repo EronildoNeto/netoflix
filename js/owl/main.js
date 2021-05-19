797cc3d9bb3618103f92249abfcaca31c4310092
EronildoNeto <eronildo.neto99@gmail.com>
2021-05-19 13:46:50 -0300

    Aquivos Finai

---
+++ js/owl/main.js
@@ -0,0 +1,7 @@
+function clicou(){
+    window.open("https://www.youtube.com/watch?v=9w-O60x1bYk&ab_channel=Simon");
+}
+
+function clicou2(){
+    window.open("https://www.themoviedb.org/tv/1425-house-of-cards?language=pt-BR");
+}
